114cbc98f4dd7ee17663da86f6f47bc27b099ad7
Summaw <98126132+Summaw@users.noreply.github.com>
2023-05-26 21:32:58 -0400

    Create main.lua
    main.lua

---
+++ main.lua
@@ -0,0 +1,22 @@
+local handsUp = false
+
+CreateThread(function()
+    while true do
+        Wait(0)
+
+        if handsUp then
+            TaskHandsUp(PlayerPedId(), 250, PlayerPedId(), -1, true)
+        end
+    end
+end)
+
+RegisterCommand('+handsup', function()
+    handsUp = true
+end, false)
+
+RegisterCommand('-handsup', function()
+    handsUp = false
+end, false)
+
+RegisterKeyMapping('+handsup', 'Hands Up', 'keyboard', 'z')
+RegisterKeyMapping('say hi', 'Say hi', 'keyboard', '`')
